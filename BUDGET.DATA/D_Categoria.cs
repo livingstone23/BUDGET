e53c4c148a6f3564eea7fc2b86347e4dbba12ab3
livingstone23 <livingstone23@gmail.com>
2021-09-23 13:39:43 +0200

    Carga inicial

---
+++ BUDGET.DATA/D_Categoria.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUDGET.DATA
+{
+    public class D_Categoria
+    {
+        SqlConnection conexion = new SqlConnection(ConfigurationManager.ConnectionStrings["conectar"].ConnectionString);
+
+    }
+}
